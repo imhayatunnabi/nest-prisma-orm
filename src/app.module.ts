d4b1b15c990e499de9083b59458bd397d1473673
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-15 14:09:21 +0600

    auth module resource authenticated

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticleModule } from './article/article.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ArticleModule, UserModule],
+  imports: [PrismaModule, ArticleModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

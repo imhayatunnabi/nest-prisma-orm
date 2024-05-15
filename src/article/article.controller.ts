110023410cd3168a7c6fc6b313e315cb7d598069
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-15 10:43:50 +0600

    Article Prisma Crud Done with Swagger Api Documentation in NestJs

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -2,32 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ArticleEntity } from './entities/article.entity';
 
 @Controller('article')
+@ApiTags('articles')
 export class ArticleController {
-  constructor(private readonly articleService: ArticleService) {}
+  constructor(private readonly articleService: ArticleService) { }
 
   @Post()
+  @ApiResponse({ type: ArticleEntity })
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articleService.create(createArticleDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
   findAll() {
     return this.articleService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
   findOne(@Param('id') id: string) {
     return this.articleService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articleService.update(+id, updateArticleDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
   remove(@Param('id') id: string) {
     return this.articleService.remove(+id);
   }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ArticleService {
+  constructor(private prisma: PrismaService) { }
   create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+    return this.prisma.article.create({ data: createArticleDto })
   }
 
   findAll() {
-    return `This action returns all article`;
+    return this.prisma.article.findMany({ where: { published: true } });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({ where: { id } });
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.article.update({ where: { id }, data: updateArticleDto });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.article.delete({ where: { id } });
   }
 }

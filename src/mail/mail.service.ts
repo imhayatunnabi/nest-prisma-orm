69ad274f317176659243d803ef4dc7a7aa68e514
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-16 18:12:30 +0600

    file permission update

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -7,17 +7,19 @@ export class MailService {
     constructor(private mailerService: MailerService) { }
 
     async sendUserWelcomeMessage(user: User) {
-        // const url = `example.com/auth/confirm?token=${token}`;
-
-        await this.mailerService.sendMail({
-            to: user.email,
-            from: '"Support Team" <support@example.com>',
-            subject: 'Welcome to Nice App!',
-            template: './confirmation',
-            context: {
-                name: user.name,
-                // url,
-            },
-        });
+        try {
+            await this.mailerService.sendMail({
+                to: user.email,
+                from: '"Support Team" <support@example.com>',
+                subject: 'Welcome to Nice App!',
+                template: './mail/templates/confirmation', // Adjust the path accordingly
+                context: {
+                    name: user.name,
+                    // Any other template variables you want to pass
+                },
+            });
+        } catch (error) {
+            console.error('Error sending welcome email:', error);
+        }
     }
 }

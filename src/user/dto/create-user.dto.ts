15db008eb2225bd2ee85365cb31942a38f76529a
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-17 18:12:05 +0600

    dummy data insert on swagger api documentation updated

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -4,20 +4,20 @@ import { IsNotEmpty, IsString, MinLength } from "class-validator";
 export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
-    @ApiProperty()
+    @ApiProperty({ default: 'imhayatunnabi' })
     name: string;
 
     @IsString()
     @IsNotEmpty()
-    @ApiProperty()
+    @ApiProperty({ default: 'imhayatunnabi@gmail.com' })
     email: string;
 
     @IsString()
     @IsNotEmpty()
     @MinLength(6)
-    @ApiProperty()
+    @ApiProperty({ default: 'P@@$$word' })
     password: string;
 
-    @ApiProperty({ type: 'string', format: 'binary', required: false })
+    @ApiProperty({ type: 'string', format: 'binary', required: false, default: 'profile-pic.png' })
     image?: any;
 }

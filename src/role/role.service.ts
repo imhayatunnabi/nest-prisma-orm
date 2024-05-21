4d36eab29ce6b492aa63bca6d500b48e2438cbc0
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-21 10:01:15 +0600

    role crud started

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RoleService {
+  constructor(private prisma: PrismaService){}
   create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+    return this.prisma.role.create({data:{
+      ...createRoleDto
+    }});
   }
 
   findAll() {
-    return `This action returns all role`;
+    return this.prisma.role.findMany();
   }
 
   findOne(id: number) {
